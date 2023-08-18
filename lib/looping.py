6e3e6005fcd47c3e6fa4cf6d631a36afcbb68f05
emmanuelmeena91 <emmanuel.meena@moringaschool.com>
2023-08-19 00:27:42 +0300

    complete assignment

--- lib/looping.py
+++ lib/looping.py
@@ -27,12 +27,12 @@ def fizzbuzzPrinter():
        fizzbuzz(i)
 
 def fizzbuzz(num):
-    if (num % 3 == 0):
-        print("Fizz")
-    elif (num % 5 == 0):
-        print("Buzz")
-    elif num % 3 == 0 and num % 5 == 0:
+    if num % 3 == 0 and num % 5 == 0:
         print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
     else:
         print(num)
 
